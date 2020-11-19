b26c6e19ed20f73d61cf1212139e9cec60eda23a
khoinguyen19k8 <khoinguyen19k8@gmail.com>
2020-11-19 23:53:51 +0100

    Turn On/Off Volumn
    Added turning volumn on/off and music

---
+++ Assets/HCI_project_V1/Scripts/TurnVolumeOn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TurnVolumeOn : MonoBehaviour
+{
+    public float totalTime = 3f;
+    private bool isLookedAt;
+    private float timeDuration = 0f;
+    public GameObject audioSource;
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        isLookedAt = gazedAt;
+        return;
+    }
+    void OnEnable()
+    {
+        SetGazedAt(false);
+    }
+    void Start()
+    {
+        SetGazedAt(false);
+    }
+    void Update()
+    {
+        if (isLookedAt)
+        {
+            timeDuration += Time.deltaTime;
+            if (timeDuration > totalTime)
+            {
+                timeDuration = 0;
+                //AudioSource audioComp = audioSource.GetComponent<AudioSource>();
+                //audioComp.volume = 1;
+                audioSource.GetComponent<AudioSource>().volume = 1;
+            }
+        }
+        else
+        {
+            timeDuration = 0;
+        }
+
+    }
+}
